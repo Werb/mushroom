f37edbfd46f3bd38571cf7698a6b928f1a35e37d
wanbo <wanbowb@live.com>
2017-07-22 11:27:31 +0800

    replace languageColor

--- mushroom/spiders/repos_spider.py
+++ mushroom/spiders/repos_spider.py
@@ -14,7 +14,7 @@ class reposSpider(scrapy.Spider):
             item['url'] = 'http://www.github.com' + repos.xpath('.//div[@class = "d-inline-block col-9 mb-1"]//h3//a/@href').extract()[0]
             item['reposName'] = repos.xpath('.//div[@class = "d-inline-block col-9 mb-1"]//h3//a/@href').extract_first()
             item['reposDesc'] = repos.xpath('normalize-space(.//p[@class = "col-9 d-inline-block text-gray m-0 pr-4"]/text())').extract_first()
-            item['languageColor'] = repos.xpath('normalize-space(.//span[@class = "repo-language-color ml-0"]/@style)').extract_first()
+            item['languageColor'] = repos.xpath('normalize-space(.//span[@class = "repo-language-color ml-0"]/@style)').extract_first()[17:24]
             item['languageType'] = repos.xpath('normalize-space(.//span[@itemprop = "programmingLanguage"]/text())').extract_first()
             item['star'] = repos.xpath('normalize-space(.//a[1][@class = "muted-link d-inline-block mr-3"])').extract_first()
             item['fork'] = repos.xpath('normalize-space(.//a[2][@class = "muted-link d-inline-block mr-3"])').extract_first()
